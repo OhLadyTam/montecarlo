5ee3a5e8a5729e617fe67071a5e3df5223dd2674
OhLadyTam <tamara.radevic.92@gmail.com>
2017-09-05 02:22:27 +0200

    added partially csv, chart functionality

--- src/clj/montecarlo/routes/home.clj
+++ src/clj/montecarlo/routes/home.clj
@@ -40,14 +40,16 @@
 (defn get-xls [ticker] (incanter.excel/save-xls (incanter.datasets/get-dataset (mcsim/start-simulation ticker)) "D:/montecarlosimulation.xls"))
 
 (defn create-dataset [ticker] (incanter.core/dataset ["col1"](mcsim/start-simulation ticker) :delim \space))
-(defn create-csv [ticker] (incanter.core/save (create-dataset ticker) "D:/d.csv"))
+
+(def ds (incanter.core/dataset ["x1" "x2"] [[1 2] [3 4]]))
+(defn create-csv [ticker] (incanter.core/save (incanter.datasets/get-dataset ds) "d.csv"))
 (defn download [ticker] (selmer.parser/render-file "montecarlosimulation.html" (create-csv ticker)))
 
 (defn open-chart1 [ticker] (incanter.core/view (incanter.charts/scatter-plot :data (incanter.datasets/get-dataset (create-dataset ticker)) :title "Montecarlo" :x-label "days" :y-label "prices")))
 
 (defn open-chart [ticker] (selmer.parser/render-file "montecarlosimulation.html" (incanter.core/view (incanter.charts/scatter-plot :data (incanter.datasets/get-dataset (incanter.core/dataset ["x1"] [[1] [2]])   :title "Montecarlo" :x-label "days" :y-label "prices")))))
 
-(defroutes home-routes (GET "/" [] (home-page)) (GET "/about" [] (about-page)) (GET "/montecarlosimulation" [] (montecarlosimulation-page)) (POST "/simulate" [ticker] (simulate-page ticker)) (POST "/getxls" [ticker] (str "da li radi") (download ticker)) (POST "/getchart" [ticker1] (open-chart ticker1)))
+(defroutes home-routes (GET "/" [] (home-page)) (GET "/about" [] (about-page)) (GET "/montecarlosimulation" [] (montecarlosimulation-page)) (POST "/simulate" [ticker] (simulate-page ticker)) (POST "/getxls" [ticker2] (download ticker2)) (POST "/getchart" [ticker1] (open-chart ticker1)))
 
 ;(incanter.core/save)
 
